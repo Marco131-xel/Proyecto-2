a6fe5f7e0865714b25e26ebb932fa63c7a56e2f5
Marco <marcopoolchichesapon@gmail.com>
2024-10-03 23:56:28 -0600

    spring boot

---
+++ captchas/src/main/java/com/company/captchas/CaptchasApplication.java
@@ -0,0 +1,13 @@
+package com.company.captchas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchasApplication.class, args);
+	}
+
+}
---
+++ captchas/src/test/java/com/company/captchas/CaptchasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.captchas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
